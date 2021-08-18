df1552415bd11136922b117e0bba3c2a3106816f
Guido22 <GuidoDulos@valori.nl>
2021-08-18 14:35:48 +0200

    Release 1.00 US001

--- src/CashRegister.java
+++ src/CashRegister.java
@@ -7,20 +7,4 @@
 
 public class CashRegister {
     private double content;
-
-    public CashRegister() {
-        content = 0.00;
-    }
-
-    public CashRegister(double content) {
-        this.content = content;
-    }
-
-    public double getContent() {
-        return content;
-    }
-
-    public void setContent(double content) {
-        this.content = content;
-    }
 }
