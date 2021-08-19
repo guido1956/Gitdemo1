aba4d6b2b8c566485788f18f3abec47f93f3dbb1
Guido22 <GuidoDulos@valori.nl>
2021-08-19 08:38:12 +0200

    US004 -Object type Loggin-

---
+++ src/Loggin.java
@@ -0,0 +1,25 @@
+public class Loggin {
+    private String logging;
+    private double value;
+
+    public Loggin(String logging, double value) {
+        this.logging = logging;
+        this.value = value;
+    }
+
+    public String getLogging() {
+        return logging;
+    }
+
+    public void setLogging(String logging) {
+        this.logging = logging;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
